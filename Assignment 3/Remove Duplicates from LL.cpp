3bc9011e4915d14eaa028e48c326a5bfccc95899
Sumit <sumit.18.paul@gmail.com>
2021-02-20 23:37:12 +0530

    Create Remove Duplicates from LL.cpp

---
+++ Assignment 3/Remove Duplicates from LL.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+	public:
+		int data;
+		Node *next;
+		Node(){
+			Node(0);
+		}
+		Node(int data){
+			this->data =  data;
+			this->next = NULL;
+		}
+};
+class SinglyLinkedList{
+	public:
+		Node* head;
+		SinglyLinkedList(){
+			this->head=NULL;
+		}
+		
+		void insertBeg(int data){
+			Node* newNode = new Node(data);
+			if(head == NULL){
+				head = newNode;
+			}
+			else{
+				newNode->next = head;
+				head = newNode;
+			}
+		}
+		
+		void insertEnd(int data){
+			Node* newNode = new Node(data);
+			if(head == NULL){
+				head = newNode;
+			}
+			else{
+				Node* ptr = head;
+				while(ptr->next != NULL){
+					ptr = ptr->next;
+				}
+				ptr->next = newNode;
+			}
+		}
+		
+		void printList(){
+			Node* ptr = head;
+			while(ptr != NULL){
+				cout<<ptr->data<<" ";
+				ptr = ptr->next;
+			}
+		}
+		
+};
+
+int getSize(Node* a){
+	int count = 0;
+	while(a != NULL){
+		count++;
+		a = a->next;
+	}
+	return count;
+}
+
+Node* removeDuplicate(Node* a){
+	if(a == NULL)
+	return NULL;
+	if(getSize(a)<=1){
+		return a;
+	}
+	Node *curr = a;
+	Node *next_next;
+	while(curr->next != NULL){
+		if(curr->data == curr->next->data){
+			next_next = curr->next->next;
+			delete curr->next;
+			curr->next = next_next;
+		}
+		else{
+			curr = curr->next;
+		}
+	}
+	return a;
+}
+
+int main(){
+	SinglyLinkedList s1, res;
+	s1.insertEnd(1);
+	s1.insertEnd(2);
+	s1.insertEnd(2);
+	s1.insertEnd(3);
+	s1.insertEnd(4);
+	res.head = removeDuplicate(s1.head);
+	res.printList();
+	return 0;
+}
