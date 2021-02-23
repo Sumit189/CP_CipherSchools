38f7ea74f4c3aaa3a94d697dcf9fc1fa8fda0b50
Sumit <sumit.18.paul@gmail.com>
2021-02-23 17:04:28 +0530

    Create  Max Sum path from the leaf to leaf.cpp

---
+++ Assignment 5/Max Sum path from the leaf to leaf.cpp
@@ -0,0 +1,24 @@
+int max(int a, int b){ 
+    return (a >= b)? a: b; 
+}
+ 
+
+int maxPathSumUtil(struct Node *root, int &res){
+    if (root==NULL) return 0;
+    if (!root->left && !root->right) return root->data;
+
+    int ls = maxPathSumUtil(root->left, res);
+    int rs = maxPathSumUtil(root->right, res);
+     
+    if (root->left && root->right){
+        res = max(res, ls + rs + root->data);
+        return max(ls, rs) + root->data;
+    }
+    return (!root->left)? rs + root->data : ls + root->data;
+}
+ 
+int maxPathSum(struct Node *root){
+    int res = INT_MIN;
+    maxPathSumUtil(root, res);
+    return res;
+}
