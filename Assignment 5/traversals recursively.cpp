4d75bae27f5c3d1423fc16e906b306314ab71dc2
Sumit <sumit.18.paul@gmail.com>
2021-02-23 10:50:21 +0530

    Update traversals recursively.cpp

--- Assignment 5/traversals recursively.cpp
+++ Assignment 5/traversals recursively.cpp
@@ -1,1 +1,25 @@
+void preorder(TreeNode *root){
+  if(root != NULL){
+  cout<<root->data<<endl;
+  preordeR(root->left);
+  preordeR(root->right);
+  }
+}
 
+
+void ineorder(TreeNode *root){
+  if(root != NULL){
+  preordeR(root->left);
+  cout<<root->data<<endl;
+  preordeR(root->right);
+  }
+}
+
+
+void postorder(TreeNode *root){
+  if(root != NULL){
+  preordeR(root->left);
+  preordeR(root->right);
+  cout<<root->data<<endl;
+  }
+}
