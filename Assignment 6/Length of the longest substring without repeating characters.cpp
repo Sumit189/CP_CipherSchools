c62b979598a471779bbc0cb5e29c11f309b83fd7
Sumit <sumit.18.paul@gmail.com>
2021-02-24 09:21:13 +0530

    Update Length of the longest substring without repeating characters.cpp

--- Assignment 6/Length of the longest substring without repeating characters.cpp
+++ Assignment 6/Length of the longest substring without repeating characters.cpp
@@ -2,30 +2,25 @@
 using namespace std;
 
 int ls(string str, int n){
+    if(str == ""){
+        return 0;
+    }
     map<char, int> mp;
+    for(int i = 0; i < n; i++)
+    mp[str[i]] = -1;
+
     int max_so_far = INT_MIN;
-    int count = 0;
-    stack<char> st;
-    for(int i = 0; i < n; i++){
-        if(mp[str[i]] == 0){
-            count++;
-            st.push(str[i]);
-            mp[str[i]]++;
-        }
-        else{
-            while(!st.empty()){
-                mp[st.top()] = 0;
-                st.pop();
-            }
-            max_so_far =  max(max_so_far, count);
-            count = 0;
-        }
+    int i = 0;
+    for(int j = 0; j < n; j++){
+        i = max(i, mp[str[j]]+1);
+        max_so_far = max(max_so_far, j-i+1);
+        mp[str[j]] = j;
     }
     return max_so_far;
 }
 
 int main(){
-    string str = "ABDEFGABEF";
+    string str = "dvdf";
     int n = str.size();
     cout<<ls(str, n);
 }
